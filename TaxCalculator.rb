2a74973626273ac97ee21dd74489da4076138d6d
Heine Morantin <h.morantin@gmail.com>
2015-08-01 22:03:37 -0400

    Commit --> 0001
    Fixing basics errors on getting data input and displaying the data after
    calculations.

--- TaxCalculator.rb
+++ TaxCalculator.rb
@@ -1,14 +1,18 @@
 # Heine Morantin Agosto 2015 
+begin
 puts "Calculadora de Impuestos de Importacion"
 puts "---------------------------------------"
-gets "Valor del artiulo en US$: " + valordolar
-gets "Tasa del US$ a RD$: " + tasadolar
+puts "Valor del artiulo en US$:"
+valordolar = gets.chomp().to_f
+puts "Tasa del US$ a RD$:"
+tasadolar = gets.chomp.to_f
 valorpesos = valordolar * tasadolar
 gravamen = valorpesos * 0.20
 selectivo = (valorpesos + gravamen) * 0.10
 itbis = (valorpesos + gravamen + selectivo) * 0.18
 impuestocourier = gravamen + selectivo + itbis
 grantotal = valorpesos + impuestocourier
-puts "Total a Pagar a la Tienda Online (RD$): " + valorpesos
-puts "Total de Impuesto a Pagar al Courier (RD$): " + impuestocourier
-puts "Gran Total (RD$): " + grantotal
+puts "Total a Pagar a la Tienda Online (RD$): " + valorpesos.to_s
+puts "Total de Impuesto a Pagar al Courier (RD$): " + impuestocourier.to_s
+puts "Gran Total (RD$): " + grantotal.to_s
+end
