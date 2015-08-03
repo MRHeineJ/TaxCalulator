96352a9edfab07dc39504d0108e3b1281b3f28a6
Heine J. Morantin <hjmorantin@users.noreply.github.com>
2015-08-03 11:34:40 -0400

    Commit --> 0002
    Coments and TODO for the week.

--- TaxCalculator.rb
+++ TaxCalculator.rb
@@ -1,4 +1,7 @@
 # Heine Morantin Agosto 2015 
+# TODO: Convertir esto en una funcion.
+# TODO: Agregar el calculo de las libras
+# TODO: Agregar una funcion que me permita seleccionar el Courier y el precio por libra.
 begin
 puts "Calculadora de Impuestos de Importacion"
 puts "---------------------------------------"
@@ -7,9 +10,9 @@ valordolar = gets.chomp().to_f
 puts "Tasa del US$ a RD$:"
 tasadolar = gets.chomp.to_f
 valorpesos = valordolar * tasadolar
-gravamen = valorpesos * 0.20
-selectivo = (valorpesos + gravamen) * 0.10
-itbis = (valorpesos + gravamen + selectivo) * 0.18
+gravamen = valorpesos * 0.20 # Al valor en pesos del articulo se le cobra un 20% de Gravamen.
+selectivo = (valorpesos + gravamen) * 0.10 # A la sumatoria del valor del arituclo y el gravamen se le cobra un 10% como selectivo al consumo.
+itbis = (valorpesos + gravamen + selectivo) * 0.18 # A la sumatoria de el valor, el gravamen y el selectivo, se le cobra un 18% de ITBIS.
 impuestocourier = gravamen + selectivo + itbis
 grantotal = valorpesos + impuestocourier
 puts "Total a Pagar a la Tienda Online (RD$): " + valorpesos.to_s
