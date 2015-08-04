ed46ad9bd9075d0849cd93d877e97083e4a67f2f
Heine Morantin <hjmorantin@icloud.com>
2015-08-04 12:24:56 -0400

    Commit --> 0003
    Tiny update on the code. An if was added for the case when an item cost
    less than US$ 200.00.

--- TaxCalculator.rb
+++ TaxCalculator.rb
@@ -1,8 +1,12 @@
-# Heine Morantin Agosto 2015 
-# Testing
-# TODO: Convertir esto en una funcion.
-# TODO: Agregar el calculo de las libras
-# TODO: Agregar una funcion que me permita seleccionar el Courier y el precio por libra.
+# @hjmorantin [08-2015]
+=begin
+TODO
+ - Convertir esto en una funcion.
+ - Agregar el calculo de las libras
+ - Agregar una funcion que me permita seleccionar el Courier y el precio por libra.
+------------------------------------------------------------------------------------
+=end
+
 begin
 puts "Calculadora de Impuestos de Importacion"
 puts "---------------------------------------"
@@ -11,12 +15,23 @@ valordolar = gets.chomp().to_f
 puts "Tasa del US$ a RD$:"
 tasadolar = gets.chomp.to_f
 valorpesos = valordolar * tasadolar
-gravamen = valorpesos * 0.20 # Al valor en pesos del articulo se le cobra un 20% de Gravamen.
-selectivo = (valorpesos + gravamen) * 0.10 # A la sumatoria del valor del arituclo y el gravamen se le cobra un 10% como selectivo al consumo.
-itbis = (valorpesos + gravamen + selectivo) * 0.18 # A la sumatoria de el valor, el gravamen y el selectivo, se le cobra un 18% de ITBIS.
-impuestocourier = gravamen + selectivo + itbis
-grantotal = valorpesos + impuestocourier
-puts "Total a Pagar a la Tienda Online (RD$): " + valorpesos.to_s
-puts "Total de Impuesto a Pagar al Courier (RD$): " + impuestocourier.to_s
-puts "Gran Total (RD$): " + grantotal.to_s
+
+if valordolar < 200
+	puts "Este articulo no paga impuestos de importación."
+	puts "Total a Pagar a la Tienda Online (RD$): " + valorpesos.to_s
+	impuestocourier = 0
+	puts "Total de Impuesto a Pagar al Courier (RD$): " + impuestocourier.to_s
+	grantotal = valorpesos + impuestocourier
+	puts "Gran Total (RD$): " + grantotal.to_s
+else
+	gravamen = valorpesos * 0.20 
+	selectivo = (valorpesos + gravamen) * 0.10 
+	itbis = (valorpesos + gravamen + selectivo) * 0.18 
+	impuestocourier = gravamen + selectivo + itbis
+	grantotal = valorpesos + impuestocourier
+	puts "Total a Pagar a la Tienda Online (RD$): " + valorpesos.to_s
+	puts "Total de Impuesto a Pagar al Courier (RD$): " + impuestocourier.to_s
+	puts "Gran Total (RD$): " + grantotal.to_s
+end
+puts "Este calculo no toma en cuenta el costo por libra del transporte."
 end
