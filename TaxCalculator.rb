8c18054a76d8b1a347b30d426caec53f0745e032
Heine J. Morantin <hjmorantin@icloud.com>
2015-08-05 16:45:00 -0400

    Commit --> 0004
    Updated TODO. Spaces were added for a better display of the information
    on the CMD.

--- TaxCalculator.rb
+++ TaxCalculator.rb
@@ -1,37 +1,56 @@
 # @hjmorantin [08-2015]
 =begin
 TODO
- - Convertir esto en una funcion.
- - Agregar el calculo de las libras
+
+ - Convert this into a class/funtion.
+ - Add the average price per pound of the courier and the math for the final price.
+ - Agregar el calculo cuando es menor de US$ 200.0
+ - Translate this to English
+ - Search for all the companies that offers courier services in DR.
+ - Add a funtion for the courier average (price per pound). 
  - Agregar una funcion que me permita seleccionar el Courier y el precio por libra.
+ - Download the tax law. (Comision tecnica de Liberativa).
 ------------------------------------------------------------------------------------
 =end
 
 begin
-puts "Calculadora de Impuestos de Importacion"
-puts "---------------------------------------"
-puts "Valor del artiulo en US$:"
-valordolar = gets.chomp().to_f
-puts "Tasa del US$ a RD$:"
-tasadolar = gets.chomp.to_f
-valorpesos = valordolar * tasadolar
-
-if valordolar < 200
-	puts "Este articulo no paga impuestos de importación."
-	puts "Total a Pagar a la Tienda Online (RD$): " + valorpesos.to_s
-	impuestocourier = 0
-	puts "Total de Impuesto a Pagar al Courier (RD$): " + impuestocourier.to_s
-	grantotal = valorpesos + impuestocourier
-	puts "Gran Total (RD$): " + grantotal.to_s
-else
-	gravamen = valorpesos * 0.20 
-	selectivo = (valorpesos + gravamen) * 0.10 
-	itbis = (valorpesos + gravamen + selectivo) * 0.18 
-	impuestocourier = gravamen + selectivo + itbis
-	grantotal = valorpesos + impuestocourier
-	puts "Total a Pagar a la Tienda Online (RD$): " + valorpesos.to_s
-	puts "Total de Impuesto a Pagar al Courier (RD$): " + impuestocourier.to_s
-	puts "Gran Total (RD$): " + grantotal.to_s
-end
-puts "Este calculo no toma en cuenta el costo por libra del transporte."
+	puts " "
+	puts "Calculadora de Impuestos de Importacion"
+	puts "---------------------------------------"
+	puts " "
+	puts "Valor del artiulo en US$:"
+	puts " "
+	valordolar = gets.chomp().to_f
+	puts " "
+	puts "Tasa del US$ a RD$:"
+	puts " "
+	tasadolar = gets.chomp.to_f
+	valorpesos = valordolar * tasadolar
+	
+	if valordolar < 200
+		puts " "
+		puts "Este articulo no paga impuestos de importación."
+		puts " "
+		puts "Total a Pagar a la Tienda Online (RD$): " + valorpesos.to_s
+		impuestocourier = 0
+		puts " "
+		puts "Total de Impuesto a Pagar al Courier (RD$): " + impuestocourier.to_s
+		grantotal = valorpesos + impuestocourier
+		puts " "
+		puts "Gran Total (RD$): " + grantotal.to_s
+	else
+		gravamen = valorpesos * 0.20 
+		selectivo = (valorpesos + gravamen) * 0.10 
+		itbis = (valorpesos + gravamen + selectivo) * 0.18 
+		impuestocourier = gravamen + selectivo + itbis
+		grantotal = valorpesos + impuestocourier
+		puts " "
+		puts "Total a Pagar a la Tienda Online (RD$): " + valorpesos.to_s
+		puts " "
+		puts "Total de Impuesto a Pagar al Courier (RD$): " + impuestocourier.to_s
+		puts " "
+		puts "Gran Total (RD$): " + grantotal.to_s
+	end
+	puts " "
+	puts "Este calculo no toma en cuenta el costo por libra del transporte."
 end
